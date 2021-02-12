d0f17612670a2580ed4a4c29a4678a62b8f44cb3
Andrei Herdt <andrei.herdt@tra.ai>
2021-02-12 22:17:16 +0000

    add writer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -51,3 +51,48 @@ pub struct Writer {
     color_code: ColorCode,
     buffer: &'static mut Buffer,
 }
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n'=>self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT -1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {}
+}
+
+impl Writer {
+    pub fn write_str(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n\ => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+}
+
+pub fn print_something{
+    writer: Writer{
+        column_position: 0,
+        color_code: ColorCode { Color::Yellow, Color::Black }
+    }
+}
